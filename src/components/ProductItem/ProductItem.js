df27679f20ace73d2048384f4cbfb0332bb34c66
sunhoh <dltjsgho94@gmail.com>
2021-10-19 10:49:35 +0900

    - Add: 공용 상품 아이템 레이아웃 - Modify: 리뷰사항 수정 - Modify: sider 수정 - Refactor: 멘토님 리뷰 반영

---
+++ src/components/ProductItem/ProductItem.js
@@ -0,0 +1,100 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+
+export default function ProductItem(props) {
+  const [toggle, setToggle] = useState(false);
+
+  return (
+    <Item>
+      <ItemImg>
+        <Save>
+          <SaveIcon
+            bookmark={toggle}
+            onClick={() => {
+              setToggle(bookmark => !bookmark);
+            }}
+          />
+        </Save>
+      </ItemImg>
+      <ItemInfo>
+        <Brand>
+          <p>Nike</p>
+        </Brand>
+        <ItemName>
+          <p>Nike Dunk Low Retro Championship Goldenrod</p>
+        </ItemName>
+        <Price>
+          <div>100,000원</div>
+          <p>즉시 구매가</p>
+        </Price>
+      </ItemInfo>
+    </Item>
+  );
+}
+
+const Item = styled.div`
+  width: 306px;
+  margin: 20px 0;
+  padding: 0 12px;
+`;
+
+const ItemImg = styled.div`
+  position: relative;
+  width: 282px;
+  height: 282px;
+  background-color: #f6eeed;
+  border-radius: 14px;
+  cursor: pointer;
+
+  &:hover {
+    & div {
+      opacity: 1;
+    }
+  }
+`;
+
+const Save = styled.div`
+  position: absolute;
+  top: 0;
+  right: 0;
+  margin: 15px;
+  font-size: 23px;
+  opacity: 0;
+  transition: all 0.2s ease;
+`;
+
+const SaveIcon = styled.i.attrs(props => ({
+  className: props.bookmark ? `fas fa-bookmark` : `far fa-bookmark`,
+}))``;
+
+const ItemInfo = styled.div`
+  padding: 15px 0 0;
+`;
+
+const Brand = styled.div`
+  p {
+    display: inline-block;
+    font-size: 16px;
+    font-weight: bold;
+    border-bottom: 2px solid black;
+  }
+`;
+
+const ItemName = styled.div`
+  margin: 10px 0 0;
+`;
+
+const Price = styled.div`
+  padding: 7px 0 0;
+
+  div {
+    font-size: 16px;
+    font-weight: bold;
+  }
+
+  p {
+    color: rgba(34, 34, 34, 0.5);
+    margin: 5px 0 0;
+    font-size: 13px;
+  }
+`;
