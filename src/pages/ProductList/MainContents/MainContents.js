6c29f6fd5e411c12beb81947d8f077ccc89e75eb
71summernight <71summernight@naver.com>
2021-10-31 22:01:08 +0900

    콘솔 지우기

--- src/pages/ProductList/MainContents/MainContents.js
+++ src/pages/ProductList/MainContents/MainContents.js
@@ -15,7 +15,6 @@ export default function MainContents({
 
   useEffect(() => {
     const searchParams = new URLSearchParams(location.search);
-    console.log(searchParams);
 
     fetch(`${BASE_URL}/products?search=${searchParams.get('search')}`)
       .then(res => res.json())
